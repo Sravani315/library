22a8b728bfe759d7714865796d74543fb36066a6
Sravani315 <sravanimedisetti15@gmail.com>
2018-07-25 15:54:38 +0530

    migration file changed

--- db/migrate/20180719090236_add_author_ref_to_books.rb
+++ db/migrate/20180719090236_add_author_ref_to_books.rb
@@ -1,6 +1,7 @@
 class AddAuthorRefToBooks < ActiveRecord::Migration[5.1]
   def change
-    add_refernces :books, :author, foreign_key: true
+    add_refernce :books, :author, foreign_key: true
     add_index :books, :author_id
   end
 end
+	
